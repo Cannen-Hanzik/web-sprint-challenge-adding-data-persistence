080d5237bb55d68067235492637fbf482b1447bd
Cannen <cannenhanzik2@gmail.com>
2022-05-20 12:12:49 -0500

    set up resource files

---
+++ api/resource/resource-middlware.js
@@ -0,0 +1,14 @@
+const db = require('../../data/dbConfig.js');
+
+module.exports = {
+    checkUniqueResourceName (req, res, next) {
+        db.get({ resource_name : req.body.resource_name })
+            .then(resource => {
+                if(resource.length > 0) return next({
+                    status: 400,
+                    message: 'Resource name already exists'
+                })
+                next()
+            })
+    }
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,28 @@
-// build your `/api/resources` router here
+const router = require('express').Router();
+const db = require('./model');
+
+const {
+    checkUniqueResourceName
+} = require('./resource-middlware');
+
+router.get('/', (req, res, next) => {
+    db.getAll()
+        .then(resource => {
+            res.status(200).json(resource);
+        })
+        .catch(next());
+});
+
+router.post('/', checkUniqueResourceName, (req, res, next) => {
+    db.add(req.body)
+        .then(newRsc => {
+            res.status(201).json(
+                newRsc[
+                    newRsc.length-1
+                ]
+            )
+        })
+        .catch(next)
+});
+
+module.exports = router;
