b256e843597a7a66bb1dace76d7f3d1fa93cbc0b
Cannen <cannenhanzik2@gmail.com>
2022-05-20 12:01:48 -0500

    set up all projects files

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,34 @@
-// build your `/api/projects` router here
+const express = require('express');
+const project = require('./model');
+const { checkProjectData } = require('./project-middlware');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    await project.getAll()
+        .then(projects => {
+            projects.map(project => {
+                project.project_completed === 0 ?
+                project.project_completed = false
+                    : project.project_completed = true
+            })
+            res.status(200).json(projects)
+        })
+        .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+    project.add(req.body)
+        .then(newPrj => {
+            if (newPrj[newPrj.length-1].project_completed == 0){
+                newPrj[newPrj.length-1].project_completed = false;
+                res.status(201).json(newPrj[newPrj.length-1]);
+            } else {
+                newPrj[newPrj.length-1].project_completed = true;
+                res.status(201).json(newPrj[newPrj.length-1]);
+            }
+        })
+        .catch(next);
+});
+
+module.exports = router;
