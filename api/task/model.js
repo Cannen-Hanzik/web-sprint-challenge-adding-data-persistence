8646bd248f83f57b831299723222b2d81bc21f2d
Cannen <cannenhanzik2@gmail.com>
2022-05-20 12:22:00 -0500

    set up tasks files

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,19 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig.js');
+
+function getAll() {
+    return db('tasks as t')
+        .select('t.*', 'p.product_name', 'p.project_description')
+        .join('projects as p', 'p.project_id', 't.project_id')
+};
+
+async function add(task) {
+    const [task_id] = await db('tasks').insert(task);
+    return getAll().where({
+        task_id
+    }).first();
+};
+
+module.exports = {
+    getAll,
+    add,
+};
